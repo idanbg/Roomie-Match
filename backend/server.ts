fcaf8fa6a209802eaedad340b8cda618e5b4fe96
idanbg <idanbengavriel@gmail.com>
2025-04-10 15:05:18 +0300

    render

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); 
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
